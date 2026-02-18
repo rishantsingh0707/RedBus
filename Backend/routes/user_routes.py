a388e59058e0cb84ecfd8af022fb1b5fc480ce48
Rishant Singh <rishant3333@example.com>
2026-02-19 00:20:48 +0530

    Added Session In Db

--- Backend/routes/user_routes.py
+++ Backend/routes/user_routes.py
@@ -1,19 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.database import SessionLocal
+from app.database import get_db
 from app.models.user import User
 from app.schemas.user_schema import UserCreate, UserResponse
 import uuid
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.post("/", response_model=UserResponse)
